9c0d7c3054390b651c10201329ebf7a4780e6499
Kathy Bagamasbad <160595766+bkathy08@users.noreply.github.com>
2024-10-22 09:33:46 +0800

    Add files via upload

---
+++ MIANA10/admin_dashboard.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+if(!isset($_SESSION['username'])||$_SESSION['role']!='admin'){
+    header("Location: index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADMIN DASHBOARD</title>
+</head>
+<body>
+    <h2>Welcome Admin</h2>
+    <?php
+    echo $_SESSION['username'];
+    ?>
+    <br>
+    <a href="logout.php">LOGOUT</a>
+</body>
+</html>
